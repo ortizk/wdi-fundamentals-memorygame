306069e98ebb4019bd7a56ccdccfe82e446c7e7c
Karla <ortizk@live.com>
2018-03-11 11:56:27 -0600

    created game logic

--- js/main.js
+++ js/main.js
@@ -31,14 +31,25 @@ var checkForMatch = function () {
 };
 };
 
-var flipCard = function (cardId) {
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
+	this.getElementById(card[cardId].cardImage).setAttribute(src);
 	if (cardsInPlay.length === 2) 
 	checkForMatch();
 };
 
-flipCard(3);
-flipCard(1);
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+	var cardElement = document.createElement('img');
+	cardElement.setAttribute('src','images/back.png');
+	data-id.setAttribute(i);
+	document.getElementById('cardElement').addEventListener('click', flipCard);
+	document.getElementById('game-board').appendChild(cardElement);
+};
+};
+
+createBoard();
