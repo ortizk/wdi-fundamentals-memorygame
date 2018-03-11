a6ba1a7adcb5caf656399e1ca4f1758008c317ec
Karla <ortizk@live.com>
2018-03-11 13:05:13 -0600

    created game logic

--- js/main.js
+++ js/main.js
@@ -45,9 +45,9 @@ var flipCard = function () {
 var createBoard = function () {
 	for (var i = 0; i < cards.length; i++) {
 	var cardElement = document.createElement('img');
-	cardElement.setAttribute('src','images/back.png');
-	data-id.setAttribute(i);
-	document.getElementById('cardElement').addEventListener('click', flipCard);
+	cardElement.setAttribute('src',"images/back.png");
+	cardElement.setAttribute('data-id','i');
+	cardElement.addEventListener('click', flipCard);
 	document.getElementById('game-board').appendChild(cardElement);
 };
 };
